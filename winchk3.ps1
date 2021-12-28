680fbfb9c93a4b816393725175aaa5bd98ad0f46
C04CH <88205891+c04ch1337@users.noreply.github.com>
2021-12-28 08:37:28 -0800

    Create winchk3.ps1
    This is the 3rd script we added. Not necessary but, we can use for any additional scripts or functions we want to execute. Future commands or other information needed.

---
+++ winchk3.ps1
@@ -0,0 +1,112 @@
+Start-Transcript -LiteralPath .\log_winchk3.log -Append
+
+function SystemInformation {
+
+}
+
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                Basic PowerShell Commands                 | "
+ Write "   +----------------------------------------------------------+ "
+
+ Write ""
+ Write "----------------------------------------------------------------------"
+ Write ""
+
+ 
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                       Get-History                        | "
+ Write "   +----------------------------------------------------------+ "
+
+
+    Write "Get-History"
+    Get-History
+    
+ Write ""
+ Write "----------------------------------------------------------------------"
+ Write ""
+
+  
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                       Get-Process                        | "
+ Write "   +----------------------------------------------------------+ "
+
+
+    Write "Get-Process"
+    Get-Process
+    
+ Write ""
+ Write "----------------------------------------------------------------------"
+ Write ""
+  
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                       Get-PSDrive                        | "
+ Write "   +----------------------------------------------------------+ "
+
+
+    Write "Get-PSDrive"
+    Get-PSDrive
+    
+ Write ""
+ Write "----------------------------------------------------------------------"
+ Write ""
+  
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                       Get-Service                        | "
+ Write "   +----------------------------------------------------------+ "
+
+
+    Write "Get-Service"
+    Get-Service
+    
+ Write ""
+ Write "----------------------------------------------------------------------"
+ Write ""
+   
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                      Get-ChildItem                       | "
+ Write "   +----------------------------------------------------------+ "
+
+
+    Write "Get-ChildItem"
+    Get-ChildItem $env:USERPROFILE
+    
+ Write ""
+ Write "----------------------------------------------------------------------"
+ Write ""
+  
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                 Windows Password Vault                   | "
+ Write "   +----------------------------------------------------------+ "
+
+
+    Write "Windows Password Vault"
+    
+
+   [Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
+    $vault = New-Object Windows.Security.Credentials.PasswordVault
+    $vault.RetrieveAll() | % { $_.RetrievePassword();$_ } | select Resource, UserName, Password | Sort-Object Resource | ft -AutoSize
+
+    
+ Write ""
+ Write "----------------------------------------------------------------------"
+ Write ""
+   
+ Write "   +----------------------------------------------------------+ "
+ Write "   |                   LIST Temp Files                        | "
+ Write "   +----------------------------------------------------------+ "
+
+
+    Write "LIST Temp Files"
+
+       dir $env:TEMP 2> $null
+        dir C:\Temp 2> $null
+        dir C:\Windows\Temp 2> $null
+
+ Write ""
+ Write "----------------------------------------------------------------------"
+
+##### KEEP ADDING COMMANDS HERE #####
+
+Stop-Transcript
+Remove-Item -Recurse -Path .\winchk3.ps1
+exit
